26427e229962c892cbce95810e2db7b22f2f1731
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2018-07-23 21:56:20 +0530

    action creator checking if logged in

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import reduxThunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 
 
